4bfc1843f9001573d0bc2866ac4417fcf30ed839
RyoDan <kensang248@gmail.com>
2019-07-07 16:28:35 +0700

    fix no cookies

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,37 +1,43 @@
-var db = require('../db.js');
+var db = require("../db.js");
 
-module.exports.index = function (request,response) {
-    let page = parseInt(request.query.page) || 1;
-    
-    let currentPage;
-    
-    let perPage = 8;
-    
-    let begin = (page-1) *perPage;
-    
-    let end =(page-1)*perPage +perPage;
-    
-    let products = db.get("products").value().slice(begin,end);
-    
-    let sessionId = request.signedCookies.sessionId;
-    let totalProduct=0;
+module.exports.index = function(request, response) {
+  let page = parseInt(request.query.page) || 1;
+
+  let currentPage;
+
+  let perPage = 8;
+
+  let begin = (page - 1) * perPage;
+
+  let end = (page - 1) * perPage + perPage;
+
+  let products = db
+    .get("products")
+    .value()
+    .slice(begin, end);
+
+  let sessionId = request.signedCookies.sessionId;
+  let totalProduct = 0;
+  if (sessionId) {
     let inCart = db
-    .get("sessions")
-    .find({ id: sessionId })
-    .value().cart;
-    
-    if(page === 1){
-        currentPage =2;
-    }else{
-        currentPage= page;
-    }
-    
+      .get("sessions")
+      .find({ id: sessionId })
+      .value().cart;
+
     for (const item in inCart) {
-        totalProduct+= inCart[item.toString()];
-      }
-    response.render("product/index.pug",{
-        products:products,
-        page : currentPage,
-        totalProduct:totalProduct
-    });
+      totalProduct += inCart[item.toString()];
+    }
+  }
+
+  if (page === 1) {
+    currentPage = 2;
+  } else {
+    currentPage = page;
+  }
+
+  response.render("product/index.pug", {
+    products: products,
+    page: currentPage,
+    totalProduct: totalProduct
+  });
 };
