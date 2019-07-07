55c20643803bfc2591354f70404b7863df820857
RyoDan <kensang248@gmail.com>
2019-07-07 16:56:41 +0700

    fix auth router

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var controller = require('../controllers/cart.controller.js');
+var controller = require('../controllers/auth.controller');
 
-router.get("/add/:productId",controller.addToCart);
-
+router.get("/login",controller.login);
+router.post("/login",controller.postLogin);
 module.exports = router;
